1d8b313b6ea267465516db424722f82ca24ea938
Dennis Dang <dennisdang5@gmail.com>
2026-04-23 03:13:32 -0700

    Finished: load_config and load_train_data in dataloader

---
+++ src/data_loader.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+import yaml
+
+def load_config(yaml_file_path='src/config.yml'):
+    project_root = Path(__file__).parent.parent
+    full_path = project_root / yaml_file_path
+    with open(full_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def load_train_data(data_config):
+    project_root = Path(__file__).parent.parent
+    full_path = project_root / data_config['train_path']
+    return pd.read_csv(
+        full_path,
+        na_values=data_config['na_values'],
+        skiprows=data_config['skip_rows']
+    )
