15105185f194027bc1cfd654c11e4dce434f98d0
Dennis Dang <dennisdang@Denniss-MacBook-Air.local>
2026-04-28 20:20:04 -0700

    Finished: load_test_data

--- src/data_loader.py
+++ src/data_loader.py
@@ -17,3 +17,12 @@ def load_train_data(data_config):
         na_values=data_config['na_values'],
         skiprows=data_config['skip_rows']
     )
+
+def load_test_data(data_config):
+    project_root = Path(__file__).parent.parent
+    full_path = project_root / data_config['test_path']
+    return pd.read_csv(
+        full_path,
+        na_values=data_config['na_values'],
+        skiprows=data_config['skip_rows']
+    )
