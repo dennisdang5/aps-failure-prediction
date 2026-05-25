e886648d2b23396da5157c2ffa7951dcf39cb26b
Dennis Dang <dennisdang5@gmail.com>
2026-05-25 16:25:27 -0700

    Added: train XGBoost Classifer function

--- src/models.py
+++ src/models.py
@@ -1,6 +1,34 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from xgboost import XGBClassifier
 
-def train_random_forest_classifier(X_train, y_train, class_weight=None, n_estimators=100, oob_score=True, random_state=42):
-    random_forest_classifier = RandomForestClassifier(n_estimators=n_estimators, oob_score=oob_score, class_weight=class_weight, random_state=random_state)
+def train_random_forest_classifier(X_train, y_train, rf_config, class_weight=None):
+    random_forest_classifier = RandomForestClassifier(
+        n_estimators=rf_config['n_estimators'],
+        oob_score=True,
+        class_weight=class_weight,
+        random_state=rf_config['random_state']
+    )
+
     random_forest_classifier.fit(X_train, y_train)
     return random_forest_classifier
+
+def train_xgb_classifier(X_train, y_train, xgb_config, cross_validation_config):
+    alphas = {'reg_alpha': xgb_config['alpha_grid']}
+    xgb_classifier = XGBClassifier(
+        booster=xgb_config['booster'],
+        objective=xgb_config['objective'],
+        random_state=xgb_config['random_state'],
+        n_jobs=xgb_config['n_jobs']
+    )
+
+    grid_search_xgb_classifier = GridSearchCV(
+        xgb_classifier,
+        alphas,
+        cv=cross_validation_config['folds'],
+        scoring=cross_validation_config['scoring']
+    )
+
+    grid_search_xgb_classifier.fit(X_train, y_train)
+    return grid_search_xgb_classifier
